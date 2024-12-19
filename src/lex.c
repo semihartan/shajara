cf9c8dc66871c66f9dac53be5f462a78364afbf1
Semih Artan <s-785412369@hotmail.com>
2024-12-19 17:42:25 +0300

    Update the lexer loop to add support for comments and quoted identifiers. Allow identifiers to have spaces inside them. Fix the logic to generate DEDENT pseudo tokens.

--- src/lex.c
+++ src/lex.c
@@ -62,6 +62,9 @@
 (c) !=  QU) || \
 _istalnum((c)))
 
+#define SUMINDENT(x)				IndentationSum(x)
+#define SUBINDENTS(x, y)		(struct Indentation_s){x.tabs - y.tabs, x.spaces - y.spaces}
+
 #define NEW_TOKEN(k, i)		(Token_t){ (TokenKind_t)k, i }
  
 static const TCHAR* s_source = NULL;
@@ -69,7 +72,16 @@ static const TCHAR* s_cp = NULL;
 static const TCHAR* s_pp;
 static struct Indentation_s { int tabs; int spaces; };
 
+
 struct Indentation_s s_previousIndentation = { 0, 0 };
+struct Indentation_s s_currentIndentation = { 0, 0 }; 
+struct Indentation_s s_firstIndentation = { 0, 0 }; 
+
+inline struct Indentation_s NullIndentation();
+inline bool IndentationIsEmpty(struct Indentation_s);
+inline int IndentationSum(struct Indentation_s indent);
+inline struct Indentation_s IndentationSub(struct Indentation_s left, struct Indentation_s right);
+inline bool IndentationIsEq(struct Indentation_s left, struct Indentation_s right);
 
 void Lex_init(const TCHAR* const _Source)
 {
@@ -77,69 +89,31 @@ void Lex_init(const TCHAR* const _Source)
 	s_cp = _Source; 
 	s_pp = s_cp;
 }
-/*
-+ A
-	+ B
-	+ C
-	+ D
-		+ F
-		+ G
-		+ H
-	+ J
-	+ I
-+ K
-+ L
-+ M
-*/
 
 Token_t Lex_NextToken()
 {
-	struct Indentation_s currentIndentation = { 0, 0 }; 
 	for (;;)
-	{ 
-		// If the current char is the beginning of file or line.
-		if (CP == s_source || PC == LF)
+	{
+		if (CC == NULLC)
+			break;
+		// If the current indentation level is less than the previous, we subtract until the previous indentation
+		// becomes equal to the current indentation. On each subtraction, we generate a DEDENT.
+		else if (SUMINDENT(s_currentIndentation) < SUMINDENT(s_previousIndentation))
 		{
-			while (CC == SP || CC == TB)
+			while (SUMINDENT(s_previousIndentation) > SUMINDENT(s_currentIndentation))
 			{
-				currentIndentation.spaces += CC == SP;
-				currentIndentation.tabs	+= CC == TB;
-				NC();
-			}
-			if (currentIndentation.spaces + currentIndentation.tabs > s_previousIndentation.spaces + s_previousIndentation.tabs) 
-			{
-				s_previousIndentation = currentIndentation;
-				return PP = CP, NEW_TOKEN(SHJ_TK_INDENT, NULL);
-			}
-			else if (currentIndentation.spaces + currentIndentation.tabs < s_previousIndentation.spaces + s_previousIndentation.tabs)
-			{
-				s_previousIndentation = currentIndentation;
+				s_previousIndentation = IndentationSub(s_previousIndentation, s_firstIndentation);
 				return PP = CP, NEW_TOKEN(SHJ_TK_DEDENT, NULL);
 			}
 		}
-		// Skip any other whitespaces.
-		while (CC == SP || CC == TB)
-		{
-			NC();
-		}
-		if (CC == PLUS)
+		// If the current char is the beginning of file or line.
+		else if (CC == PLUS)
 			return PP = CP, NC(), NEW_TOKEN(SHJ_TK_PLUS, NULL);
 		else if (CC == LF)
 		{
 			PP = CP;
 			NC();
-		}
-		else if (IS_VALID_FILE_CHAR(CC))
-		{
-			const TCHAR* idStart = CP++;
-			size_t len = 1;
-			while (IS_VALID_FILE_CHAR(CC))
-			{
-				len++;
-				NC();
-			}
-			return NEW_TOKEN(SHJ_TK_ID, string_newLen(idStart, len));
-		}
+		}		
 		else if (CC == _T('#') || CC == _T('/'))
 		{
 			if (CC == _T('/'))
@@ -150,11 +124,107 @@ Token_t Lex_NextToken()
 				else
 					continue;
 			}
-			while (CC != LF) 
+			while (CC != LF && CC != NULLC)
 				NC();
 		}
-		else if (CC == _T('\0'))
-			break;
+		else if (IS_VALID_FILE_CHAR(CC) || CC == _T('"') || CC == _T('\''))
+		{
+			bool hasStartQuote = false;
+			if (CC == _T('"') || CC == _T('\''))
+			{
+				hasStartQuote = true;
+				NC();
+			}
+			const TCHAR* idStart = CP++;
+			size_t len = 1;
+			while (true)
+			{
+				if (hasStartQuote) 
+				{
+					if(CC == _T('"') || CC == _T('\''))
+					{
+						NC();
+						break;
+					}
+					
+				}
+				else
+				{
+					if (CC == LF || CC == NULLC || CC == HASH || CC == FSL)
+						break;
+				}
+				SHJ_HALT_IF(CC != SP && !IS_VALID_FILE_CHAR(CC),
+					printe("File name must include valid file characters."););
+				len++;
+				NC();
+			}
+			string_t id = string_newLen(idStart, len); 
+			for (int i = len-1; i >= 0; --i)
+			{
+				if (id[i] != SP)
+				{
+					id[i + 1] = NULLC;
+					STRING_LEN(id) = i + 1;
+					break;
+				}
+			}
+			return NEW_TOKEN(SHJ_TK_ID, id);
+		}
+		else if (CP == s_source || PC == LF)
+		{
+			s_currentIndentation = NullIndentation();
+			while (CC == SP || CC == TB)
+			{
+				s_currentIndentation.spaces += CC == SP;
+				s_currentIndentation.tabs += CC == TB;
+				NC();
+			} 
+			if (IndentationSum(s_currentIndentation) > IndentationSum(s_previousIndentation))
+			{
+				
+				if (IndentationIsEmpty(s_firstIndentation))
+				{
+					s_firstIndentation = s_currentIndentation;
+				}
+				else
+					SHJ_HALT_IF(!IndentationIsEq(IndentationSub(s_currentIndentation, s_previousIndentation), s_firstIndentation),
+				printe("Inconsistent tab and space uses."););
+				s_previousIndentation = s_currentIndentation;
+				return PP = CP, NEW_TOKEN(SHJ_TK_INDENT, NULL);
+			}
+		}
+		else if(CC == SP || CC == TB)
+		{
+			// Skip any other whitespaces.
+			while (CC == SP || CC == TB)
+				NC(); 
+		}
 	}
 	return NEW_TOKEN(SHJ_TK_EOF, NULL);
 }
+
+inline struct Indentation_s NullIndentation()
+{
+	struct Indentation_s result = { 0 };
+	return result;
+}
+inline bool IndentationIsEmpty(struct Indentation_s indentation)
+{
+	return indentation.spaces == 0 && indentation.tabs == 0;
+}
+inline int IndentationSum(struct Indentation_s indent)
+{
+	return indent.spaces + indent.tabs;
+}
+inline struct Indentation_s IndentationSub(struct Indentation_s left, struct Indentation_s right)
+{
+	struct Indentation_s result = { 0 };
+	result.tabs = left.tabs - right.tabs;
+	result.spaces = left.spaces - right.spaces;
+
+	return result;
+};
+inline bool IndentationIsEq(struct Indentation_s left, struct Indentation_s right)
+{
+	return left.tabs == right.tabs && left.spaces == right.spaces;
+};
